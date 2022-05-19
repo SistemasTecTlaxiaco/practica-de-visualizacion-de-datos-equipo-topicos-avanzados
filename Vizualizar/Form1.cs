c5d9882fe5c6c70d4bf2b76634d6390adbad1649
Maria <Majolopezleyva@gmail.com>
2022-05-19 10:02:47 -0500

    Buscar Datos

--- Vizualizar/Form1.cs
+++ Vizualizar/Form1.cs
@@ -45,11 +45,52 @@ namespace Vizualizar
             }
         }
 
-      
+        void buscar()
+        {
+            try
+            {
+                string query = "SELECT * FROM `datos alumnos` WHERE `No.Control` = " + textBoxnocontrol.Text;
+                Console.WriteLine(query);
+                MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+                commandDatabase.CommandTimeout = 60;
+                reader = commandDatabase.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        textBoxnocontrol.Text = reader.GetString(1);
+                        textBoxnombre.Text = reader.GetString(2);
+                        textBoxapellidoP.Text = reader.GetString(3);
+                        textBoxapellidoM.Text = reader.GetString(4);
+                        textBoxsemestre.Text = reader.GetString(5);
+                        textBoxemail.Text = reader.GetString(6);
+                    }
+                }
+                else
+                {
+                    textBoxnocontrol.Text = "";
+                    textBoxnombre.Text = "";
+                    textBoxapellidoP.Text = "";
+                    textBoxapellidoM.Text = "";
+                    textBoxsemestre.Text = "";
+                    textBoxemail.Text = "";
+                    MessageBox.Show("No se encontro nada");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No hay nada");
+            }
+        }
+
+
 
         private void buttonbuscar_Click(object sender, EventArgs e)
         {
             conectar();
+            buscar();
         }
 
         private void Form1_Load(object sender, EventArgs e)
